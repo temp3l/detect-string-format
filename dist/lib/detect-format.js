a17ff3033899a7898683c69e02b03954bb2ca999
steph <stephanmettler@gmx.de>
2020-01-25 20:47:14 +0100

    pass configs or use defaults

--- dist/lib/detect-format.js
+++ dist/lib/detect-format.js
@@ -5,20 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const ajv_1 = __importDefault(require("ajv"));
 //  See: https://github.com/epoberezkin/ajv for more information!
-// detected-formats:
-let enabled = ["date", "time", "date-time", "uri", "url", "email", "ipv4", "ipv6", "uuid"];
-// you can enable more Formats, like:
-//  [ "hostname", "json-pointer", "json-pointer-uri-fragment", "relative-json-pointer", "uri-reference", "regex" ]
-// or write your own patterns:
-// The following example matches a simple North American telephone number with an optional area code:
-const customSchemas = [
-    {
-        type: "string",
-        pattern: "^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$"
-    }
-];
-const allFormats = [...enabled, ...customSchemas];
-const options = {
+const ajvOptions = {
     allErrors: true,
     verbose: true,
     format: "full",
@@ -26,35 +13,44 @@ const options = {
     jsonPointers: true
 };
 const onlyUnique = (value, index, self) => self.indexOf(value) === index; // filter array
-const getInstances = ({ allFormats, options }) => allFormats.map(schema => {
-    if (typeof schema === "string")
-        return new ajv_1.default(options).compile({ format: schema });
-    else
-        return new ajv_1.default(options).compile(schema);
-});
-exports.returnFormats = (values) => {
-    const instances = getInstances({ allFormats, options });
+const getInstances = ({ schemas, options }) => schemas.map(schema => new ajv_1.default(options).compile(schema));
+// detect-formats:
+const defaultFormats = ["date", "time", "date-time", "uri", "url", "email", "ipv4", "ipv6", "uuid"];
+// you can enable more Formats, like:
+//  [ "hostname", "json-pointer", "json-pointer-uri-fragment", "relative-json-pointer", "uri-reference", "regex" ]
+// or write your own patterns:
+// The following example matches a simple North American telephone number with an optional area code:
+// See https://json-schema.org/ for more information
+const sampleSchemas = [
+    {
+        type: "string",
+        pattern: "^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$"
+    },
+    ...defaultFormats.map((format) => ({ type: "string", format }))
+];
+exports.returnFormats = (values, schemas = sampleSchemas, options = ajvOptions) => {
+    const instances = getInstances({ schemas, options });
     return instances
         .map((validate, i) => {
         const passed = values.map((data) => validate(data));
         const allMatch = passed.filter(onlyUnique);
-        if (allMatch.length === 1 && allMatch[0] === true)
-            return allFormats[i];
+        if (schemas && allMatch.length === 1 && allMatch[0] === true)
+            return schemas[i];
     })
         .filter(d => d);
 };
 // Samples
-// console.log({
-//   urlsFormat: returnFormats(["https://www.example.com/foo/?bar=baz&inga=42&quux", "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com", "http://foo.com/unicode_(✪)_in_parens", "https://github.com/epoberezkin/ajv/blob/master/lib/compile/formats.js"]),
-//   ipv4: returnFormats(["0.0.0.0", "127.0.0.53", "127.0.0.1", "192.168.1.13", "0.0.0.0", "1.2.3.4"]),
-//   ipv6: returnFormats(["fe00::0", "ff02::3", "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "fe80::f2de:f1ff:fe55:53"]),
-//   dates: returnFormats(["1963-06-19", "2020-10-02"]),
-//   dateTime: returnFormats(["12:34:56.789", "12:34:56+01:00", "12:34:56"]),
-//   TelPass: returnFormats(["555-1212", "(888)555-1212"]),
-//   TelFail: returnFormats(["(888)555-1212 ext. 532", "(800)FLOWERS"]),
-//   URI: returnFormats(["scheme:[//authority]path[?query][#fragment]", "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", "#fragment"]),
-//   someString: returnFormats(["foo", "bar", "baz"])
-// });
+console.log({
+    urlsFormat: exports.returnFormats(["https://www.example.com/foo/?bar=baz&inga=42&quux", "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com", "http://foo.com/unicode_(✪)_in_parens", "https://github.com/epoberezkin/ajv/blob/master/lib/compile/formats.js"]),
+    ipv4: exports.returnFormats(["0.0.0.0", "127.0.0.53", "127.0.0.1", "192.168.1.13", "0.0.0.0", "1.2.3.4"]),
+    ipv6: exports.returnFormats(["fe00::0", "ff02::3", "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "fe80::f2de:f1ff:fe55:53"]),
+    dates: exports.returnFormats(["1963-06-19", "2020-10-02"]),
+    dateTime: exports.returnFormats(["12:34:56.789", "12:34:56+01:00", "12:34:56"]),
+    TelPass: exports.returnFormats(["555-1212", "(888)555-1212"]),
+    TelFail: exports.returnFormats(["(888)555-1212 ext. 532", "(800)FLOWERS"]),
+    URI: exports.returnFormats(["scheme:[//authority]path[?query][#fragment]", "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", "#fragment"]),
+    someString: exports.returnFormats(["foo", "bar", "baz"])
+});
 /*
 // Will return:
 {
--- lib/detect-format.ts
+++ lib/detect-format.ts
@@ -1,9 +1,19 @@
 import Ajv from "ajv";
+import { JSONSchema7 } from "json-schema";
 
 //  See: https://github.com/epoberezkin/ajv for more information!
+const ajvOptions: Ajv.Options = {
+  allErrors: true,
+  verbose: true,
+  format: "full",
+  inlineRefs: false,
+  jsonPointers: true
+};
+const onlyUnique = (value: any, index: number, self: any[]) => self.indexOf(value) === index; // filter array
+const getInstances = ({ schemas, options }: { schemas: JSONSchema7[]; options: Ajv.Options }) => schemas.map(schema => new Ajv(options).compile(schema));
 
-// detected-formats:
-let enabled: any = ["date", "time", "date-time", "uri", "url", "email", "ipv4", "ipv6", "uuid"];
+// detect-formats:
+const defaultFormats: string[] = ["date", "time", "date-time", "uri", "url", "email", "ipv4", "ipv6", "uuid"];
 
 // you can enable more Formats, like:
 //  [ "hostname", "json-pointer", "json-pointer-uri-fragment", "relative-json-pointer", "uri-reference", "regex" ]
@@ -11,56 +21,42 @@ let enabled: any = ["date", "time", "date-time", "uri", "url", "email", "ipv4", 
 // or write your own patterns:
 // The following example matches a simple North American telephone number with an optional area code:
 
-const customSchemas = [
+// See https://json-schema.org/ for more information
+const sampleSchemas: JSONSchema7[] = [
   {
     type: "string",
     pattern: "^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$"
-  }
+  },
+  ...defaultFormats.map((format): JSONSchema7 => ({ type: "string", format }))
 ];
-const allFormats = [...enabled, ...customSchemas];
 
-const options: Ajv.Options = {
-  allErrors: true,
-  verbose: true,
-  format: "full",
-  inlineRefs: false,
-  jsonPointers: true
-};
-
-const onlyUnique = (value: any, index: number, self: any[]) => self.indexOf(value) === index; // filter array
-const getInstances = ({ allFormats, options }: { allFormats: string[]; options: Ajv.Options }) =>
-  allFormats.map(schema => {
-    if (typeof schema === "string") return new Ajv(options).compile({ format: schema });
-    else return new Ajv(options).compile(schema);
-  });
-
-export const returnFormats = (values: string[]) => {
-  const instances = getInstances({ allFormats, options });
+export const returnFormats = (values: string[], schemas = sampleSchemas, options = ajvOptions) => {
+  const instances = getInstances({ schemas, options });
   return instances
     .map((validate, i) => {
       const passed = values.map((data: string) => validate(data));
       const allMatch = passed.filter(onlyUnique);
-      if (allMatch.length === 1 && allMatch[0] === true) return allFormats[i];
+      if (schemas && allMatch.length === 1 && allMatch[0] === true) return schemas[i];
     })
     .filter(d => d);
 };
 
 // Samples
-// console.log({
-//   urlsFormat: returnFormats(["https://www.example.com/foo/?bar=baz&inga=42&quux", "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com", "http://foo.com/unicode_(✪)_in_parens", "https://github.com/epoberezkin/ajv/blob/master/lib/compile/formats.js"]),
+console.log({
+  urlsFormat: returnFormats(["https://www.example.com/foo/?bar=baz&inga=42&quux", "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com", "http://foo.com/unicode_(✪)_in_parens", "https://github.com/epoberezkin/ajv/blob/master/lib/compile/formats.js"]),
 
-//   ipv4: returnFormats(["0.0.0.0", "127.0.0.53", "127.0.0.1", "192.168.1.13", "0.0.0.0", "1.2.3.4"]),
-//   ipv6: returnFormats(["fe00::0", "ff02::3", "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "fe80::f2de:f1ff:fe55:53"]),
+  ipv4: returnFormats(["0.0.0.0", "127.0.0.53", "127.0.0.1", "192.168.1.13", "0.0.0.0", "1.2.3.4"]),
+  ipv6: returnFormats(["fe00::0", "ff02::3", "2001:0db8:85a3:0000:0000:8a2e:0370:7334", "fe80::f2de:f1ff:fe55:53"]),
 
-//   dates: returnFormats(["1963-06-19", "2020-10-02"]),
-//   dateTime: returnFormats(["12:34:56.789", "12:34:56+01:00", "12:34:56"]),
+  dates: returnFormats(["1963-06-19", "2020-10-02"]),
+  dateTime: returnFormats(["12:34:56.789", "12:34:56+01:00", "12:34:56"]),
 
-//   TelPass: returnFormats(["555-1212", "(888)555-1212"]),
-//   TelFail: returnFormats(["(888)555-1212 ext. 532", "(800)FLOWERS"]),
+  TelPass: returnFormats(["555-1212", "(888)555-1212"]),
+  TelFail: returnFormats(["(888)555-1212 ext. 532", "(800)FLOWERS"]),
 
-//   URI: returnFormats(["scheme:[//authority]path[?query][#fragment]", "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", "#fragment"]),
-//   someString: returnFormats(["foo", "bar", "baz"])
-// });
+  URI: returnFormats(["scheme:[//authority]path[?query][#fragment]", "https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", "#fragment"]),
+  someString: returnFormats(["foo", "bar", "baz"])
+});
 
 /*
 // Will return:
